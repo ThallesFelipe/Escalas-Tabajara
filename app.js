32ad3987bcb55919b57cf3610a4e3f0f7a5670fa
Thalles F. <94018271+ThallesFelipe@users.noreply.github.com>
2025-04-21 23:27:20 -0300

    Revert "refactor: aplicar estilos de glassmorphism em componentes da interface"
    This reverts commit 72a23a9353c8a50840266d48fd2e71a45d032f97.

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ const getMondayDate = (date) => {
 const createScheduleColumn = (id, dayName, schedule, cleaningDate) => {
     const column = document.createElement('div');
     column.id = id;
-    column.className = 'column glass'; // Adiciona glassmorphism
+    column.className = 'column';
 
     const columnHeader = document.createElement('header');
     const title = document.createElement('h3');
